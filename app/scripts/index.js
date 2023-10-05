7e5b75abd2b393e602f68bf1d37517d340e18ade
kevin.garcia <kevin.garcia@sinapsis.global>
2023-10-05 00:31:19 -0500

    implementing features

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,12 +1,48 @@
-let citas = []
 const contenedor = document.getElementById("tarjetas");
+const inpt = document.querySelectorAll("input")
+const displayCards = () => {
+    contenedor.innerHTML = ""
+    citas.forEach((cards) => {
+        const card = document.createElement("div");
+        card.setAttribute("id", "card-contaier")
+        const h3Name = document.createElement("h3");
+        h3Name.textContent = cards.nombre;
+        const pFecha = document.createElement("p");
+        pFecha.textContent = cards.fecha;
+        const pHora = document.createElement("p");
+        pHora.textContent = cards.hora;
+        card.appendChild(h3Name);
+        card.appendChild(pFecha);
+        card.appendChild(pHora);
+        contenedor.appendChild(card);
+    })
+}
+const noCitas = () => {
+    contenedor.innerHTML = ""
+    const noCit = document.createElement("h2")
+    noCit.textContent = "No Tienes Citas";
+    contenedor.appendChild(noCit)
+}
+let citas = "citas" in localStorage ? JSON.parse(localStorage.getItem("citas")) : []
+if (citas.length > 0) {
+    displayCards()
+} else {
+    noCitas()
+}
 const agendar = document.getElementById("agendar");
 const inpNombre = document.querySelector(".nombre");
 const inpFecha = document.querySelector(".fecha");
 agendar.addEventListener("click", (e) => {
     e.preventDefault();
     if (!inpNombre.value || !inpFecha.value) {
-        alert("Rellene Todos Los Campos")
+        inpt.forEach(inp => {
+            inp.classList.add("error")
+        })
+        setTimeout(() => {
+            inpt.forEach(inp => {
+                inp.classList.remove("error")
+            })
+        }, 2000);
     } else {
         let nuevaCita = {
             nombre: "",
@@ -26,23 +62,3 @@ agendar.addEventListener("click", (e) => {
         displayCards()
     }
 })
-
-const displayCards = () => {
-    contenedor.innerHTML = ""
-    citas.forEach((cards) => {
-        const card = document.createElement("div");
-        card.setAttribute("id", "card-contaier")
-        const h3Name = document.createElement("h3");
-        h3Name.textContent = cards.nombre;
-        const pFecha = document.createElement("p");
-        pFecha.textContent = cards.fecha;
-        const pHora = document.createElement("p");
-        pHora.textContent = cards.hora;
-        card.appendChild(h3Name);
-        card.appendChild(pFecha);
-        card.appendChild(pHora);
-        contenedor.appendChild(card);
-    })
-}
-
-
