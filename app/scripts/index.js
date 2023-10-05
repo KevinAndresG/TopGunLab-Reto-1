028ded5e3042357f1e8d20b6a2a840ed49d59b18
kevin.garcia <kevin.garcia@sinapsis.global>
2023-10-05 09:00:08 -0500

    implementing delete

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,33 +1,41 @@
 const contenedor = document.getElementById("tarjetas");
 const inpt = document.querySelectorAll("input")
 let citas = "citas" in localStorage ? JSON.parse(localStorage.getItem("citas")) : []
+
 const displayCards = () => {
     contenedor.innerHTML = ""
-    citas.forEach((cards) => {
-        const card = document.createElement("div");
-        card.setAttribute("id", "card-contaier")
-        const h3Name = document.createElement("h3");
-        h3Name.textContent = cards.nombre;
-        const pFecha = document.createElement("p");
-        pFecha.textContent = cards.fecha;
-        const pHora = document.createElement("p");
-        pHora.textContent = cards.hora;
-        card.appendChild(h3Name);
-        card.appendChild(pFecha);
-        card.appendChild(pHora);
-        contenedor.appendChild(card);
-    })
+    if (citas.length > 0) {
+        citas.forEach((cards, i) => {
+            const card = document.createElement("div");
+            card.setAttribute("id", "card-contaier")
+            const del = document.createElement("span");
+            del.textContent = "X"
+            del.addEventListener("click", () => {
+                citas.splice(i, 1);
+                localStorage.setItem("citas", JSON.stringify(citas))
+                displayCards();
+            })
+            const h3Name = document.createElement("h3");
+            h3Name.textContent = cards.nombre;
+            const pFecha = document.createElement("p");
+            pFecha.textContent = cards.fecha;
+            const pHora = document.createElement("p");
+            pHora.textContent = cards.hora;
+            card.appendChild(h3Name);
+            card.appendChild(pFecha);
+            card.appendChild(pHora);
+            card.insertBefore(del, card.firstChild);
+            contenedor.appendChild(card);
+        })
+    } else {
+        noCitas()
+    }
 }
 const noCitas = () => {
     contenedor.innerHTML = ""
     const noCit = document.createElement("h2")
     noCit.textContent = "No Tienes Citas";
     contenedor.appendChild(noCit)
-}
-if (citas.length > 0) {
-    displayCards()
-} else {
-    noCitas()
 }
 const agendar = document.getElementById("agendar");
 const inpNombre = document.querySelector(".nombre");
@@ -64,3 +72,4 @@ agendar.addEventListener("click", (e) => {
         displayCards()
     }
 })
+displayCards()
